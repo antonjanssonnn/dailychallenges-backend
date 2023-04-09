856eac23493f3c63f95647fbcff42ce8050cea39
antonjanssonnn <anton46304@gmail.com>
2023-04-09 13:29:56 +0200

    Fixed error wit server.js

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const WebSocket = require('ws');
+const http = require('http');
 
-//Routes
+// Routes
 const usersRouter = require('./routes/users');
 const challengesRouter = require('./routes/challenges');
 const friendsRouter = require('./routes/friends');
@@ -14,8 +15,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const wss = new WebSocket.Server({ noServer: true });
-
+const server = http.createServer(app); // Create an HTTP server
+const wss = new WebSocket.Server({ server }); // Attach WebSocket server to the HTTP server
 
 const uri = process.env.MONGODB_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -31,7 +32,8 @@ app.use('/challenges', challengesRouter);
 app.use('/friends', friendsRouter);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+
+server.listen(PORT, () => { // Use the HTTP server to listen instead of the Express app
   console.log(`Server is running on port: ${PORT}`);
 });
 
