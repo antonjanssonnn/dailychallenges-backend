8c940c9a1e4ee14233db923aa03d0a7d2bc2cd5a
Anton <anton46304@gmail.com>
2023-04-13 18:19:16 +0200

    Fixed the payload issue

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const liveFeedRouter = require('./routes/livefeed');
 dotenv.config();
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 
 const server = http.createServer(app); // Create an HTTP server
 const wss = new WebSocket.Server({ server }); // Attach WebSocket server to the HTTP server
